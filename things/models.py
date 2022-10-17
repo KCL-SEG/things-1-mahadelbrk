0dc7f567ad6013b97641b848573e9123f60c3dc7
mahadelbrk <k20032965@kcl.ac.uk>
2022-10-17 13:15:18 +0100

    show things: Exercise 2

--- things/models.py
+++ things/models.py
@@ -1,4 +1,6 @@
 from enum import unique
+from django.core.validators import MinValueValidator, MaxValueValidator
+from wsgiref.validate import validator
 from django.db import models
 #from django.contrib.auth.models import AbstractUser
 
@@ -10,6 +12,4 @@ class Thing(models.Model):
 
     name = models.CharField(unique=True, blank=False, max_length= 30)
     description = models.CharField(unique=False, blank=True, max_length= 120)
-    quantity = models.IntegerField(unique=False)
-
-    #test
+    quantity = models.IntegerField(unique=False, validators= [MinValueValidator(0), MaxValueValidator(100)])
