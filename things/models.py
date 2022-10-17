47571ef453f4fcb814f6a469ad81ee71d17e6728
mahadelbrk <k20032965@kcl.ac.uk>
2022-10-17 12:31:46 +0100

    show things: Exercise 2

--- things/models.py
+++ things/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # name: a short string that identifies a thing.
 # description: a slightly longer string that describes a thing.
 # quantity: an integer that identifies the number of items of a thing we possess.
 
-class Thing(models.Model):
+class Thing(AbstractUser):
 
     name = models.TextField()
     description = models.TextField()
